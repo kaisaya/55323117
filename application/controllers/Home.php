d7be0efa324a969601f1016a059be63327a02321
ASUS <kaisaya@gmail.com>
2016-11-26 15:59:32 +0700

    add boostrap work

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -9,8 +9,13 @@
 class Home extends CI_Controller
 {
     Public function index(){
-
+        $this->load->view('layout/header_view');
         $this->load->view('home_view');
+
+   }
+    Public function test(){
+        $this->load->view('layout/header_view');
+        $this->load->view('test_view');
 
     }
 }
---
+++ application/views/test_view.php
@@ -0,0 +1,11 @@
+
+<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid cupiditate eligendi hic impedit ipsa iste
+    itaque, iure minus modi nulla officia placeat quidem quisquam quo saepe tempora vel velit.
+</div>
+<div>Accusantium at commodi doloribus ea facilis impedit neque nihil quo similique voluptatibus? Ab commodi delectus,
+    deleniti dolore ducimus eius eveniet ex inventore labore mollitia quam quasi quis totam vel voluptate?
+</div>
+<div>Aliquam aliquid assumenda consectetur cum dolor eum, exercitationem hic illo incidunt ipsa itaque iure iusto minus
+    molestias nobis odio officiis perferendis quidem reprehenderit, unde veritatis voluptas voluptate! Atque,
+    exercitationem fugit?
+</div>
