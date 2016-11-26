03f3802328d4c6cd8263c0d1a250577dd620ef80
ASUS <kaisaya@gmail.com>
2016-11-26 11:37:02 +0700

    add ci

---
+++ Forrmlogin.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+<style type="text/css">
+body {
+	margin-left: 0px;
+	margin-top: 0px;
+	margin-right: 0px;
+	margin-bottom: 0px;
+}
+</style>
+</head>
+
+<body>
+<br>
+<br>
+<br>
+<br>
+<form name="form1" method="post" action="login.php">
+  <table width="50%" border="0" align="center" cellpadding="0" cellspacing="0">
+    <tr>
+      <td width="27%" height="26" align="center">Username</td>
+      <td width="73%" colspan="2"><label for="Username"></label>
+      <input type="text" name="Username" id="Username"></td>
+    </tr>
+    <tr>
+      <td align="center">Password</td>
+      <td colspan="0"><label for="pass"></label>
+      <input type="password" name="pass" id="pass">        
+      <label for="textarea"></label></td>
+    </tr>
+    <tr>
+      <td height="0" colspan="3" align="center"><input type="submit" name="button2" id="button2" value="login"></td>
+    </tr>
+  </table>
+</form>
+</body>
+</html>
